106e4ef976e19eabe140c43a38c39a94559c4a94
arullintalgat36@gmail.com <arullintalgat36@gmail.com>
2022-01-24 09:07:07 +0300

    AddSt

--- BlazorYuar/Data/Student.cs
+++ BlazorYuar/Data/Student.cs
@@ -11,29 +11,34 @@ namespace BlazorYuar.Data
         public string Sur { get; set; }
         public string Name { get; set; }
         public int Year { get; set; }
-        public int Kurs { get; set; }
-        
-        
-        public Student(string sur, string name, int year, int kurs)
+        public string Faculty { get; set; }
+        public static List<Student> st = new List<Student>();
+
+        public Student(string sur, string name, int year, string faculty)
         {            
-            sur = Sur;
-            name = Name;
-            year = Year;
-            kurs = Kurs;          
+            Sur = sur;
+            Name = name;
+            Year = year;
+            Faculty = faculty;          
         }
 
+        public static void AddToList(string surnamep, string namep, int year, string facultyp)
+        {
+            Student std = new Student(surnamep, namep, year, facultyp);
+            st.Add(std);
+        }
         public Student()
         {
         }
 
         public static List<Student> AddSt()
         {
-            List<Student> st = new List<Student>();
-            st.Add(new Student() { Sur = "Иванов", Name = "Иван", Year = 16, Kurs = 1});
-            st.Add(new Student() { Sur = "Смирнов", Name = "Иван", Year = 17, Kurs = 2});
-            st.Add(new Student() { Sur = "Иванова", Name = "Марина", Year = 18, Kurs = 3});
-            st.Add(new Student() { Sur = "Кушин", Name = "Александр", Year = 16, Kurs = 1});
-            st.Add(new Student() { Sur = "Рожкова", Name = "Мария", Year = 17, Kurs = 2});
+            //List<Student> st = new List<Student>();
+            st.Add(new Student() { Sur = "Иванов", Name = "Иван", Year = 16, Faculty = "Программист" });
+            st.Add(new Student() { Sur = "Смирнов", Name = "Иван", Year = 17, Faculty = "Программист" });
+            st.Add(new Student() { Sur = "Иванова", Name = "Марина", Year = 18, Faculty = "Программист" });
+            st.Add(new Student() { Sur = "Кушин", Name = "Александр", Year = 16, Faculty = "Программист" });
+            st.Add(new Student() { Sur = "Рожкова", Name = "Мария", Year = 17, Faculty = "Программист" });
             return st;            
         }
     }
